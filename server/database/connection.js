6c096ab3cb3d737eebb4c266a4e3d65a439273d9
eldeveloper90 <ali.elsese104@gmail.com>
2023-03-27 15:32:41 +0200

    database

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI)
+    mongoose.connect(process.env.DB_URI)
     .then((conn) => {
         console.log(`Database Connected: ${conn.connection.host}`);
     })
