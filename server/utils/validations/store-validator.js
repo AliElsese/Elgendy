5e329748d6ee9e5b62e45b0511028522adf4d334
eldeveloper90 <ali.elsese104@gmail.com>
2023-03-30 05:19:52 +0200

    updatestore

--- server/utils/validations/store-validator.js
+++ server/utils/validations/store-validator.js
@@ -2,12 +2,12 @@ const { check } = require('express-validator');
 const ApiError = require('../apiError');
 const validatorMiddleware = require('../../middlewares/validator-middleware');
 
-const StoreModel = require('../../models/store-model')
+const ProductModel = require('../../models/product-model')
 
 exports.addStoreProductValidator = [
     check('proCode').notEmpty().withMessage('كود الصنف مطلوب')
     .custom(async (val) => {
-        await StoreModel.findOne({ proCode: val }).then((product) => {
+        await ProductModel.findOne({ proCode: val }).then((product) => {
             if(!product) {                
                 return Promise.reject('كود الصنف غير موجود قم بادخاله اولا');
             }
